f8e1cdf256884ea8d076fb8f7c7c291f24acf0e3
hyi <hongyi@renci.org>
2021-10-05 22:11:26 -0400

    added auto load of test data when standing up containers in the local development environment

--- girder/restore_db.sh
+++ girder/restore_db.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    docker cp dump_data.gz girder-mongodb:dump_data.gz
+else
+    docker cp $1 girder-mongodb:dump_data.gz	
+fi    
 docker exec girder-mongodb sh -c 'exec mongorestore --gzip --archive < dump_data.gz'
--- girder/up.sh
+++ girder/up.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker-compose up --build -d -V 
+docker-compose up --build -d -V
+echo "INFO: allow girder-server and girder-mongodb containers to stand up before restoring data"
+for pc in $(seq 10 -1 1); do
+    echo -ne "$pc ...\033[0K\r" && sleep 1;
+done
+sh restore_db.sh
