eae394c5ea71227fe0b4ce6f3849286fe743b4ba
hyi <hongyi@renci.org>
2021-11-02 16:56:04 -0400

    trying to get docker working for production

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose up --build -d -V
-echo "INFO: allow girder-server and girder-mongodb containers to stand up before restoring d
+echo "INFO: allow girder and girder-mongodb containers to stand up before restoring d
 ata"
 for pc in $(seq 10 -1 1); do
     echo -ne "$pc ...\033[0K\r" && sleep 1;
