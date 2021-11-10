f6c118ae7126c56e02a3fb253af4b76ae34f2a55
David <borland@renci.org>
2021-11-10 16:00:46 -0500

    Show slice of visible human data

--- client/src/vtk/slice-view.js
+++ client/src/vtk/slice-view.js
@@ -1,116 +1,147 @@
 import { useState, useRef, useEffect } from 'react';
 
-import '@kitware/vtk.js/Rendering/Profiles/Geometry';
+import '@kitware/vtk.js/Rendering/Profiles/Volume';
+
+// Force DataAccessHelper to have access to various data source
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HtmlDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
 
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
+import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
+import vtkImageSlice from '@kitware/vtk.js/Rendering/Core/ImageSlice';
+import vtkInteractorStyleManipulator from '@kitware/vtk.js/Interaction/Style/InteractorStyleManipulator';
 
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkConeSource      from '@kitware/vtk.js/Filters/Sources/ConeSource';
+import Manipulators from '@kitware/vtk.js/Interaction/Manipulators';
+
+const { SlicingMode } = vtkImageMapper;
 
 export const SliceView = () => {
   const vtkContainerRef = useRef(null);
   const context = useRef(null);
-  const [coneResolution, setConeResolution] = useState(6);
-  const [representation, setRepresentation] = useState(2);
 
   useEffect(() => {
     if (!context.current) {
-      const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
-        rootContainer: vtkContainerRef.current,
+      const reader = vtkHttpDataSetReader.newInstance({
+        fetchGzip: true,
       });
-      const coneSource = vtkConeSource.newInstance({ height: 1.0 });
 
-      const mapper = vtkMapper.newInstance();
-      mapper.setInputConnection(coneSource.getOutputPort());
+      reader
+        .setUrl(`https://kitware.github.io/vtk-js/data/volume/headsq.vti`, { loadData: true })
+        .then(() => {      
+          const data = reader.getOutputData();
+          const range = data.getPointData().getScalars().getRange();
+          const extent = data.getExtent();          
+          
+          const imageMapper = vtkImageMapper.newInstance();
+          imageMapper.setInputData(data);
+          imageMapper.setSlicingMode(SlicingMode.K);
+          imageMapper.setSlice((extent[5] - extent[4]) / 2);
+                             
+          const imageActor = vtkImageSlice.newInstance();
+          imageActor.getProperty().setInterpolationTypeToNearest();
+          imageActor.getProperty().setColorLevel((range[1] - range[0]) / 2);
+          imageActor.getProperty().setColorWindow(range[1] - range[0]);
+          imageActor.setMapper(imageMapper);
+          
+          const wMin = 1;
+          const wMax = range[1] - range[0];
+          const wGet = imageActor.getProperty().getColorWindow;
+          const wSet = w => {
+            imageActor.getProperty().setColorWindow(w);
+          };
+          const lMin = range[0];
+          const lMax = range[1];
+          const lGet = imageActor.getProperty().getColorLevel;
+          const lSet = l => {
+            imageActor.getProperty().setColorLevel(l);
+          };
+          const kMin = extent[4];
+          const kMax = extent[5];
+          const kGet = imageMapper.getSlice;
+          const kSet = k => {
+            imageMapper.setSlice(k);
+          };
 
-      const actor = vtkActor.newInstance();
-      actor.setMapper(mapper);
+          const fullScreenRenderWindow = vtkFullScreenRenderWindow.newInstance({
+            rootContainer: vtkContainerRef.current,
+            background: [0, 0, 0],
+          });
+    
+          const renderWindow = fullScreenRenderWindow.getRenderWindow();
+          const renderer = fullScreenRenderWindow.getRenderer();  
+          renderer.getActiveCamera().setParallelProjection(true);
 
-      const renderer = fullScreenRenderer.getRenderer();
-      const renderWindow = fullScreenRenderer.getRenderWindow();
+          const manipulator = Manipulators.vtkMouseRangeManipulator.newInstance({
+            button: 1,
+            scrollEnabled: true,
+          });
+          manipulator.setVerticalListener(wMin, wMax, 1, wGet, wSet, 1);
+          manipulator.setHorizontalListener(lMin, lMax, 1, lGet, lSet, 1);
+          manipulator.setScrollListener(kMin, kMax, 1, kGet, kSet, 1);
 
-      renderer.addActor(actor);
-      renderer.resetCamera();
-      renderWindow.render();
+          const interactorStyle = vtkInteractorStyleManipulator.newInstance();
+          interactorStyle.addMouseManipulator(manipulator);
 
-      context.current = {
-        fullScreenRenderer,
-        renderWindow,
-        renderer,
-        coneSource,
-        actor,
-        mapper,
-      };
-    }
+          fullScreenRenderWindow.getInteractor().setInteractorStyle(interactorStyle);
+
+          renderer.addActor(imageActor);
+
+          renderer.resetCamera();
+          renderer.resetCameraClippingRange();
+          renderWindow.render();
+    
+          context.current = {
+            reader,
+            fullScreenRenderWindow,
+            renderWindow,
+            renderer,
+            interactorStyle,
+            manipulator,
+            imageMapper,
+            imageActor
+          };
+        });
+    }  
 
     return () => {
       if (context.current) {
-        const { fullScreenRenderer, coneSource, actor, mapper } = context.current;
-        actor.delete();
-        mapper.delete();
-        coneSource.delete();
-        fullScreenRenderer.delete();
+        const { reader, fullScreenRenderWindow, interactorStyle, manipulator, imageMapper, imageActor } = context.current;
+
+        imageActor.delete();
+        imageMapper.delete();
+        manipulator.delete();
+        interactorStyle.delete();
+        fullScreenRenderWindow.delete();
+        reader.delete();
+
         context.current = null;
       }
     };
   }, [vtkContainerRef]);
 
-  useEffect(() => {
-    if (context.current) {
-      const { coneSource, renderWindow } = context.current;
-      coneSource.setResolution(coneResolution);
-      renderWindow.render();
-    }
-  }, [coneResolution]);
+/*  
+    useEffect(() => {
+        if (context.current) {
+            const { coneSource, renderWindow } = context.current;
+            coneSource.setResolution(coneResolution);
+            renderWindow.render();
+        }
+    }, [coneResolution]);
 
-  useEffect(() => {
-    if (context.current) {
-      const { actor, renderWindow } = context.current;
-      actor.getProperty().setRepresentation(representation);
-      renderWindow.render();
-    }
-  }, [representation]);
+    useEffect(() => {
+        if (context.current) {
+            const { actor, renderWindow } = context.current;
+            actor.getProperty().setRepresentation(representation);
+            renderWindow.render();
+        }
+    }, [representation]);
+*/    
 
   return (
     <div style={{ height: 500 }}>
-      <div ref={vtkContainerRef} />
-      <table
-        style={{
-          position: 'absolute',
-          top: '25px',
-          left: '25px',
-          background: 'white',
-          padding: '12px',
-        }}
-      >
-        <tbody>
-          <tr>
-            <td>
-              <select
-                value={representation}
-                style={{ width: '100%' }}
-                onInput={(ev) => setRepresentation(Number(ev.target.value))}
-              >
-                <option value="0">Points</option>
-                <option value="1">Wireframe</option>
-                <option value="2">Surface</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input
-                type="range"
-                min="4"
-                max="80"
-                value={coneResolution}
-                onChange={(ev) => setConeResolution(Number(ev.target.value))}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div ref={ vtkContainerRef } />
     </div>
   );
 };
