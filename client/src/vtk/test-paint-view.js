0c2ce991fc380f2b56ffa0c77a93d99cc902d399
David <borland@renci.org>
2022-01-19 16:59:05 -0500

    Set initial mask for painter

--- client/src/vtk/test-paint-view.js
+++ client/src/vtk/test-paint-view.js
@@ -5,8 +5,6 @@ import '@kitware/vtk.js/Rendering/Profiles/All';
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 import vtkWidgetManager from '@kitware/vtk.js/Widgets/Core/WidgetManager';
 import vtkPaintWidget from '@kitware/vtk.js/Widgets/Widgets3D/PaintWidget';
-import vtkRectangleWidget from '@kitware/vtk.js/Widgets/Widgets3D/RectangleWidget';
-import vtkEllipseWidget from '@kitware/vtk.js/Widgets/Widgets3D/EllipseWidget';
 import vtkSplineWidget from '@kitware/vtk.js/Widgets/Widgets3D/SplineWidget';
 import vtkInteractorStyleImage  from '@kitware/vtk.js/Interaction/Style/InteractorStyleImage';
 import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
@@ -15,14 +13,7 @@ import vtkPaintFilter from '@kitware/vtk.js/Filters/General/PaintFilter';
 import vtkColorTransferFunction from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction';
 import vtkPiecewiseFunction  from '@kitware/vtk.js/Common/DataModel/PiecewiseFunction';
 
-import {
-  BehaviorCategory,
-  ShapeBehavior,
-} from '@kitware/vtk.js/Widgets/Widgets3D/ShapeWidget/Constants';
-
 import { ViewTypes } from '@kitware/vtk.js/Widgets/Core/WidgetManager/Constants';
-
-import { vec3 } from 'gl-matrix';
 
 import { DataContext } from '../contexts';
 import { useResize } from '../hooks';
@@ -83,27 +74,6 @@ export const TestPaintView = () => {
       // Widgets
       const widgets = {};
       widgets.paintWidget = vtkPaintWidget.newInstance();
-      widgets.rectangleWidget = vtkRectangleWidget.newInstance({
-        resetAfterPointPlacement: true,
-      });
-      widgets.ellipseWidget = vtkEllipseWidget.newInstance({
-        resetAfterPointPlacement: true,
-      });
-      widgets.circleWidget = vtkEllipseWidget.newInstance({
-        resetAfterPointPlacement: true,
-        modifierBehavior: {
-          None: {
-            [BehaviorCategory.PLACEMENT]:
-              ShapeBehavior[BehaviorCategory.PLACEMENT].CLICK_AND_DRAG,
-            [BehaviorCategory.POINTS]: ShapeBehavior[BehaviorCategory.POINTS].RADIUS,
-            [BehaviorCategory.RATIO]: ShapeBehavior[BehaviorCategory.RATIO].FREE,
-          },
-          Control: {
-            [BehaviorCategory.POINTS]:
-              ShapeBehavior[BehaviorCategory.POINTS].DIAMETER,
-          },
-        },
-      });
       widgets.splineWidget = vtkSplineWidget.newInstance({
         resetAfterPointPlacement: true,
       });
@@ -114,18 +84,6 @@ export const TestPaintView = () => {
 
       scene.paintHandle = scene.widgetManager.addWidget(
         widgets.paintWidget,
-        ViewTypes.SLICE
-      );
-      scene.rectangleHandle = scene.widgetManager.addWidget(
-        widgets.rectangleWidget,
-        ViewTypes.SLICE
-      );
-      scene.ellipseHandle = scene.widgetManager.addWidget(
-        widgets.ellipseWidget,
-        ViewTypes.SLICE
-      );
-      scene.circleHandle = scene.widgetManager.addWidget(
-        widgets.circleWidget,
         ViewTypes.SLICE
       );
       scene.splineHandle = scene.widgetManager.addWidget(
@@ -140,7 +98,7 @@ export const TestPaintView = () => {
       scene.splineHandle.setOutputBorder(true);
       scene.polygonHandle.setOutputBorder(true);
 
-      scene.widgetManager.grabFocus(widgets.splineWidget);
+      scene.widgetManager.grabFocus(widgets.polygonWidget);
 
       // Paint filter
       const painter = vtkPaintFilter.newInstance();
@@ -207,7 +165,8 @@ export const TestPaintView = () => {
       scene.renderer.addViewProp(labelMap.actor);
 
       // update paint filter
-      painter.setBackgroundImage(image.data);
+      painter.setBackgroundImage(imageData);
+      painter.setLabelMap(maskData);
       // don't set to 0, since that's our empty label color from our pwf
       painter.setLabel(1);
       // set custom threshold
@@ -221,19 +180,6 @@ export const TestPaintView = () => {
 
       // set 2D camera position
       setCamera(sliceMode, scene.renderer, image.data);
-
-      // set text display callback
-      scene.circleHandle.onInteractionEvent(() => {
-        const worldBounds = scene.circleHandle.getBounds();
-
-        const text = `radius: ${(
-          vec3.distance(
-            [worldBounds[0], worldBounds[2], worldBounds[4]],
-            [worldBounds[1], worldBounds[3], worldBounds[5]]
-          ) / 2
-        ).toFixed(2)}`;
-        widgets.circleWidget.getWidgetState().getText().setText(text);
-      });
 
       scene.splineHandle.setHandleSizeInPixels(
         2 * Math.max(...image.data.getSpacing())
@@ -261,18 +207,12 @@ export const TestPaintView = () => {
           data.indexToWorld(ijk, position);
 
           widgets.paintWidget.getManipulator().setOrigin(position);
-          widgets.rectangleWidget.getManipulator().setOrigin(position);
-          widgets.ellipseWidget.getManipulator().setOrigin(position);
-          widgets.circleWidget.getManipulator().setOrigin(position);
           widgets.splineWidget.getManipulator().setOrigin(position);
           widgets.polygonWidget.getManipulator().setOrigin(position);
 
           painter.setSlicingMode(slicingMode);
 
           scene.paintHandle.updateRepresentationForRender();
-          scene.rectangleHandle.updateRepresentationForRender();
-          scene.ellipseHandle.updateRepresentationForRender();
-          scene.circleHandle.updateRepresentationForRender();
           scene.splineHandle.updateRepresentationForRender();
           scene.polygonHandle.updateRepresentationForRender();
 
@@ -310,72 +250,6 @@ export const TestPaintView = () => {
         painter.addPoint(widgets.paintWidget.getWidgetState().getTrueOrigin());
       });
       initializeHandle(scene.paintHandle);
-
-      scene.rectangleHandle.onEndInteractionEvent(() => {
-        const rectangleHandle = scene.rectangleHandle
-          .getWidgetState()
-          .getRectangleHandle();
-
-        const origin = rectangleHandle.getOrigin();
-        const corner = rectangleHandle.getCorner();
-
-        if (origin && corner) {
-          painter.paintRectangle(origin, corner);
-        }
-      });
-      initializeHandle(scene.rectangleHandle);
-
-      scene.ellipseHandle.onEndInteractionEvent(() => {
-        const center = scene.ellipseHandle
-          .getWidgetState()
-          .getEllipseHandle()
-          .getOrigin();
-        const point2 = scene.ellipseHandle
-          .getWidgetState()
-          .getPoint2Handle()
-          .getOrigin();
-
-        if (center && point2) {
-          let corner = [];
-          if (
-            scene.ellipseHandle.isBehaviorActive(
-              BehaviorCategory.RATIO,
-              ShapeBehavior[BehaviorCategory.RATIO].FIXED
-            )
-          ) {
-            const radius = vec3.distance(center, point2);
-            corner = [radius, radius, radius];
-          } else {
-            corner = [
-              center[0] - point2[0],
-              center[1] - point2[1],
-              center[2] - point2[2],
-            ];
-          }
-
-          painter.paintEllipse(center, corner);
-        }
-      });
-      initializeHandle(scene.ellipseHandle);
-
-      scene.circleHandle.onEndInteractionEvent(() => {
-        const center = scene.circleHandle
-          .getWidgetState()
-          .getEllipseHandle()
-          .getOrigin();
-        const point2 = scene.circleHandle
-          .getWidgetState()
-          .getPoint2Handle()
-          .getOrigin();
-
-        if (center && point2) {
-          const radius = vec3.distance(center, point2);
-          const corner = [radius, radius, radius];
-
-          painter.paintEllipse(center, corner);
-        }
-      });
-      initializeHandle(scene.circleHandle);
 
       scene.splineHandle.onEndInteractionEvent(() => {
         const points = scene.splineHandle.getPoints();
