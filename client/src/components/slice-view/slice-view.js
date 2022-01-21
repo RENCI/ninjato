cc7647443da238db733515cb0a58e163bbbf7a17
David <borland@renci.org>
2022-01-21 17:07:01 -0500

    Add scroll to change slice

--- client/src/components/slice-view/slice-view.js
+++ client/src/components/slice-view/slice-view.js
@@ -1,11 +1,13 @@
 import '@kitware/vtk.js/Rendering/Profiles/All';
 
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
-import vtkInteractorStyleImage  from '@kitware/vtk.js/Interaction/Style/InteractorStyleImage';
+import vtkInteractorStyleManipulator from '@kitware/vtk.js/Interaction/Style/InteractorStyleManipulator';
 import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
 import { Widgets } from './widgets';
 import { Image } from './image';
 import { Mask } from './mask';
+
+import Manipulators from '@kitware/vtk.js/Interaction/Manipulators';
 
 const slicingMode = vtkImageMapper.SlicingMode.K;
 
@@ -28,6 +30,11 @@ export function SliceView(onEdit) {
   let renderer = null;
   let camera = null;
 
+  const manipulator = Manipulators.vtkMouseRangeManipulator.newInstance({
+    button: 1,
+    scrollEnabled: true,
+  });
+
   const image = Image();
   const mask = Mask();  
   const widgets = Widgets(mask.getPainter(), onEdit);
@@ -47,21 +54,43 @@ export function SliceView(onEdit) {
       camera = renderer.getActiveCamera();
       camera.setParallelProjection(true);
 
-      const style = vtkInteractorStyleImage.newInstance();
-      style.setInteractionMode('IMAGE_SLICING');
-      renderWindow.getInteractor().setInteractorStyle(style);
+      const interactorStyle = vtkInteractorStyleManipulator.newInstance();
+      interactorStyle.addMouseManipulator(manipulator);
+
+      renderWindow.getInteractor().setInteractorStyle(interactorStyle);
 
       widgets.setRenderer(renderer);
     },
     setData: (imageData, maskData) => {
-      image.setInputData(imageData);
+      image.setInputData(imageData);    
+      mask.setInputData(imageData, maskData);
 
       renderer.addViewProp(image.getActor());
       renderer.addViewProp(mask.getActor());
     
-      mask.setInputData(imageData, maskData);
-    
       setCamera(slicingMode, renderer, imageData);
+
+      const range = imageData.getPointData().getScalars().getRange();
+      const extent = imageData.getExtent(); 
+
+      const wMin = 1;
+      const wMax = range[1] - range[0];
+      const wGet = image.getActor().getProperty().getColorWindow;
+      const wSet = w => image.getActor().getProperty().setColorWindow(w);
+
+      const lMin = range[0];
+      const lMax = range[1];
+      const lGet = image.getActor().getProperty().getColorLevel;
+      const lSet = l => image.getActor().getProperty().setColorLevel(l);
+
+      const kMin = extent[4];
+      const kMax = extent[5];
+      const kGet = image.getMapper().getSlice;
+      const kSet = k => image.getMapper().setSlice(k);
+
+      manipulator.setVerticalListener(wMin, wMax, 1, wGet, wSet, 1);
+      manipulator.setHorizontalListener(lMin, lMax, 1, lGet, lSet, 1);
+      manipulator.setScrollListener(kMin, kMax, 1, kGet, kSet, 1);
     
       const update = () => {  
         const ijk = [0, 0, 0];
