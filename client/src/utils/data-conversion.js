8b6680de7a5ec2f541b8a8ec8dc66ff30f1b9b62
David <borland@renci.org>
2022-01-25 10:45:16 -0500

    Progress toward multifile tiff saving

--- client/src/utils/data-conversion.js
+++ client/src/utils/data-conversion.js
@@ -3,6 +3,58 @@ import utif from 'utif';
 import vtkImageData from '@kitware/vtk.js/Common/DataModel/ImageData';
 import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
 
+
+const encodeImage = (image, w, h, n, bpp = 16) => {
+  const stripByteCounts = w * h * bpp / 8;
+  const arrayType = bpp === 32 ? Uint32Array : bpp === 16 ? Uint16Array : Uint8Array;
+
+  // Augment utif tag types with some we need
+  utif.ttypes[254] = 4;
+  utif.ttypes[297] = 5;
+ 
+  const idf = { 
+    t254: [2],                // subfile type
+    t256: [w],                // image width
+    t257: [h],                // image height
+    t258: [bpp],              // bits per sample
+    t259: [1],                // compression
+    t262: [1],                // photometric interpretation
+    t274: [1],                // orientation
+    t277: [1],                // samples per pixel
+    t278: [h],                // rows per strip
+    t279: [stripByteCounts],  // strip byte counts
+    t282: [10],               // x resolution
+    t283: [10],               // y resolution
+    t284: [1],                // planar configuration
+    t296: [3]                 // resolution unit
+  };
+
+  const headerOffset = 10000;
+  let offset = headerOffset;
+  const idfs = [];
+  for (let i = 0; i < n; i++) {
+    idf.t273 = [offset];      // strip offsets
+    idf.t297 = [i, n];        // page number
+
+    idfs.push({...idf});
+
+    offset += stripByteCounts;
+  }
+
+  console.log(idfs);
+	
+	const prfx = new arrayType(utif.encode(idfs));
+	const img = new arrayType(image);
+  const data = new arrayType(headerOffset + stripByteCounts);
+  
+	for(let i = 0; i < prfx.length; i++) data[i] = prfx[i];
+  for(let i = 0; i < img.length; i++) data[headerOffset + i] = img[i];
+  
+	return data.buffer;
+}
+
+/*
+
 // Based on encodeImage from utif, but adjusted for 16-bit single component images
 const encodeImage = function(image, w, h, z, n, bpp = 16)
 {
@@ -23,6 +75,8 @@ const encodeImage = function(image, w, h, z, n, bpp = 16)
   
 	return data.buffer;
 }
+
+*/
 
 export const readTIFF = buffer => {
 
@@ -71,11 +125,7 @@ export const readTIFF = buffer => {
 }; 
 
 export const writeTIFF = image => {
-  console.log(image);
-
-  //image.modified();
-
-  const [width, height] = image.getDimensions();
+  const [width, height, depth] = image.getDimensions();
   const [min, max] = image.getPointData().getScalars().getRange();
 
   console.log(width, height);
@@ -85,23 +135,11 @@ export const writeTIFF = image => {
   console.log(image.getPointData());
   console.log(image.getPointData().getScalars());
 
-  console.log(data);
-
-  const rgba = [];
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      const v = data[x * height + y] / max * 255;
-      rgba.push(v, v, v, 255);
-    }
-  }
-
-  console.log(rgba);
-
-  const buffer = utif.encodeImage(rgba, width, height);
+  const buffer = encodeImage(data, width, height, depth);
 
   console.log(buffer);
 
-  const blob = new Blob([buffer], { type: "image/tiff" });
+  const blob = new Blob([buffer], { type: 'image/tiff' });
 
   const url = URL.createObjectURL(blob);
 
