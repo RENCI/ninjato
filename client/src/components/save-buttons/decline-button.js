5d6ba2774694895f2a58daa5959b14b213ed2b11
David Borland <borland@renci.org>
2022-02-03 20:36:10 -0500

    Feature/submit abandon buttons (#23)
    * Add success message via popup
    
    * Fix some warnings
    
    * Add dialogs for submit and abandon buttons
    
    * Change abandon to decline
    
    * Change save button appearance
    
    * Add files via upload
    
    * Add files via upload
    
    * Adjust decline text

---
+++ client/src/components/save-buttons/decline-button.js
@@ -0,0 +1,92 @@
+import { useContext, useState, useRef } from 'react';
+import { Button, Modal, Icon } from 'semantic-ui-react';
+import { UserContext, DataContext, CLEAR_DATA } from 'contexts';
+//import { api } from 'utils/api';
+
+const { Header, Content, Actions } = Modal;
+
+export const DeclineButton = ({ disabled }) => {
+  // const [{ id, assignment }] = useContext(UserContext);
+  const [, dataDispatch] = useContext(DataContext);
+  const [showModal, setShowModal] = useState(false);
+  const [declining, setDeclining] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const ref = useRef();
+
+  const onDecline = () => {
+    setShowModal(true);
+  };
+
+  const onConfirm = async () => {
+    setDeclining(true);
+
+    try {
+      // XXX: Need an decline endpoint
+      // await api.saveAnnotations(id, assignment.itemId, blob, true);
+
+      setSuccess(true);
+      setTimeout(() => {        
+        setSuccess(false);
+        setShowModal(false);
+
+        dataDispatch({ type: CLEAR_DATA });
+      }, 2000);
+    }
+    catch (error) {
+      console.log(error);        
+    }
+
+    setDeclining(false); 
+  };
+
+  return (
+    <>
+      <Button 
+        ref={ ref }
+        negative
+        disabled={ disabled }
+        onClick={ onDecline }
+      >
+        Decline
+      </Button>
+      <Modal
+        dimmer='blurring'
+        open={ showModal }        
+      >
+        <Header>Decline Region</Header>
+        <Content>
+          { declining ?             
+            <>Processing</>
+          :  success ?
+            <>
+              <Icon name='check circle outline' color='green' />
+              Declined successfully
+            </>
+          :
+            <>
+              <p>Decline current region?</p>
+              <p>All edits will be lost.</p>
+            </>
+          }
+        </Content>
+        <Actions>
+          <Button 
+            negative 
+            disabled={ declining || success }
+            onClick={ () => setShowModal(false) }
+          >
+            Cancel
+          </Button>
+          <Button 
+            positive 
+            disabled={ declining || success }
+            loading={ declining }
+            onClick={ onConfirm } 
+          >
+            Confirm
+          </Button>
+        </Actions>
+      </Modal>
+    </>
+  );
+};
--- client/src/components/save-buttons/save-button.js
+++ client/src/components/save-buttons/save-button.js
@@ -2,12 +2,12 @@ import { useContext, useState, useRef } from 'react';
 import { Popup, Button, Icon } from 'semantic-ui-react';
 import { UserContext, DataContext } from 'contexts';
 import { api } from 'utils/api';
-import { encodeTIFF } from 'utils/data-conversion';
+import { encodeTIFF, saveTIFF } from 'utils/data-conversion';
 
 // Download for testing
 const download = false;
 
-export const SaveButton = ({ text, color, done = false }) => {
+export const SaveButton = ({ disabled, onSaving }) => {
   const [{ id, assignment }] = useContext(UserContext);
   const [{ maskData }] = useContext(DataContext);
   const [saving, setSaving] = useState(false);
@@ -15,39 +15,29 @@ export const SaveButton = ({ text, color, done = false }) => {
   const ref = useRef();
 
   const onSave = async () => {
+    setSaving(true);
+    onSaving(true);
+
     const buffer = encodeTIFF(maskData);
+
+    if (download) {  
+      saveTIFF(buffer, 'testTiff.tif');
+    }
 
     const blob = new Blob([buffer], { type: 'image/tiff' });
 
-    if (download) {  
-      const url = URL.createObjectURL(blob);
-  
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = 'testTIFF.tif';
+    try {
+      await api.saveAnnotations(id, assignment.itemId, blob);
 
-      document.body.appendChild(a);
-      a.click();
-      
-      window.URL.revokeObjectURL(url); 
-      a.remove();
+      setSuccess(true);
+      setTimeout(() => setSuccess(false), 2000);
     }
-    else {
-      setSaving(true);
-
-      try {
-        await api.saveAnnotations(id, assignment.itemId, blob, done);
-
-        setSuccess(true);
-        setTimeout(() => setSuccess(false), 2000);
-      }
-      catch (error) {
-        console.log(error);        
-      }
-
-      setSaving(false);
+    catch (error) {
+      console.log(error);        
     }
+
+    setSaving(false);
+    onSaving(false);
   };
 
   return (
@@ -57,17 +47,19 @@ export const SaveButton = ({ text, color, done = false }) => {
       trigger={ 
         <Button 
           ref={ ref }
-          color={ color }
+          basic
+          color='green'
+          disabled={ disabled}
           loading={ saving }
           onClick={ onSave }
         >
-          { text }
+          Save
         </Button>
         }
       content={ 
         <>
           <Icon name='check circle outline' color='green' />
-          { done ? <>Submitted successfully</> : <>Saved successfully</> }          
+          Saved successfully
         </>
       }
     />
