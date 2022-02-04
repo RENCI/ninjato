83abcfa79cb18ff25c0e9cda74500fdbb418fe57
David <borland@renci.org>
2022-02-04 11:25:36 -0500

    Add assigment-selection component

---
+++ client/src/components/assignment-selection/assignment-selection.js
@@ -0,0 +1,56 @@
+import { useContext } from 'react';
+import { Grid, Segment, Message, Button, Divider } from 'semantic-ui-react';
+import { UserContext } from 'contexts';
+import { useLoadData, useLoadPracticeData } from 'hooks';
+
+const { Row, Column } = Grid;
+
+export const AssignmentSelection = () => {
+  const [{ login, assignment }] = useContext(UserContext);
+  const loadData = useLoadData();
+  const loadPracticeData = useLoadPracticeData();
+
+  const onLoadClick = () => {
+    loadData(assignment);
+  };
+
+  return (
+    <>
+      { login ?
+        <Grid >
+          <Row>
+            <Column width={ 2 } >            
+            </Column>
+            <Column width={ 12 }>
+              <Segment>
+                <h3>Welcome { login }!</h3>
+                { assignment ? 
+                  <Message>
+                    <Message.Header>
+                      You have an assignment waiting for you
+                    </Message.Header>
+                    <div style={{ marginTop: 10 }}>
+                      <Button primary onClick={ onLoadClick }>Load assignment</Button>
+                      <Divider horizontal>Or</Divider>
+                      <Button secondary onClick={ loadPracticeData }>Load practice data</Button>
+                    </div>
+                  </Message>
+                :
+                  <Message>
+                    <Message.Header>
+                      No assignment found!
+                    </Message.Header>
+                    <div style={{ marginTop: 10 }}>
+                      <Button secondary onClick={ loadPracticeData }>Load practice data</Button>
+                    </div>
+                  </Message>              
+                }
+              </Segment>
+            </Column>
+          </Row>
+        </Grid>
+      : null
+      }
+    </>
+  );
+};
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,61 +1,17 @@
 import { useContext } from 'react';
-import { Grid, Segment, Message, Button, Divider } from 'semantic-ui-react';
-import { UserContext, DataContext } from 'contexts';
-import { useLoadData, useLoadPracticeData } from 'hooks';
+import { DataContext } from 'contexts';
+import { AssignmentSelection } from 'components/assignment-selection';
 import { VisualizationContainer } from 'components/visualization-container';
 
-const { Row, Column } = Grid;
-
 export const Home = () => {
-  const [{ login, assignment }] = useContext(UserContext);
   const [{ imageData }] = useContext(DataContext);
-  const loadData = useLoadData();
-  const loadPracticeData = useLoadPracticeData();
-
-  const onLoadClick = () => {
-    loadData(assignment);
-  };
 
   return (
     <>
-      { imageData ?
-        <>
-          <VisualizationContainer />
-        </>
-      : login ?
-        <Grid >
-          <Row>
-            <Column width={ 2 } >            
-            </Column>
-            <Column width={ 12 }>
-              <Segment>
-                <h3>Welcome { login }!</h3>
-                { assignment ? 
-                  <Message>
-                    <Message.Header>
-                      You have an assignment waiting for you
-                    </Message.Header>
-                    <div style={{ marginTop: 10 }}>
-                      <Button primary onClick={ onLoadClick }>Load assignment</Button>
-                      <Divider horizontal>Or</Divider>
-                      <Button secondary onClick={ loadPracticeData }>Load practice data</Button>
-                    </div>
-                  </Message>
-                :
-                  <Message>
-                    <Message.Header>
-                      No assignment found!
-                    </Message.Header>
-                    <div style={{ marginTop: 10 }}>
-                      <Button secondary onClick={ loadPracticeData }>Load practice data</Button>
-                    </div>
-                  </Message>              
-                }
-              </Segment>
-            </Column>
-          </Row>
-        </Grid>
-      : null
+      { imageData ? 
+        <VisualizationContainer />        
+      : 
+        <AssignmentSelection />
       }
     </>
   );
