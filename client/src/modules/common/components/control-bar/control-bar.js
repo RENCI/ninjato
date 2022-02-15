3fe8f0375226e98eefb0d53795134eb9064be703
David <borland@renci.org>
2022-02-14 20:20:20 -0500

    Update control bar styles

--- client/src/modules/common/components/control-bar/control-bar.js
+++ client/src/modules/common/components/control-bar/control-bar.js
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 export const ControlBar = ({ children }) => {
   return (
-    <div className={ styles.buttons }>
+    <div className={ styles.controlBar }>
       { children }
     </div>
   );
