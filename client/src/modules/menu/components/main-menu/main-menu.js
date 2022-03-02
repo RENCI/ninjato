4e879f41e67dcc2f1b8801100f27ad57fb2824e9
David <borland@renci.org>
2022-03-02 11:29:36 -0500

    Adjust menu

--- client/src/modules/menu/components/main-menu/main-menu.js
+++ client/src/modules/menu/components/main-menu/main-menu.js
@@ -1,19 +1,17 @@
-import { Container, Menu } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { UserControls } from 'modules/menu/components/user-controls';
 
 const { Item } = Menu;
 
 export const MainMenu = () => (
-  <Menu stackable borderless inverted attached size='large'>
-    <Container>
-      <Item as={ Link } to='/' header>
-        <img alt="logo" src='logo192.png' />
-        ninjatō
-      </Item>
-      <Menu borderless inverted floated='right'>
-        <UserControls />
-      </Menu>
-    </Container>
+  <Menu borderless inverted attached>
+    <Item as={ Link } to='/' header>
+      <img alt="logo" src='logo192.png' />
+      ninjatō
+    </Item>
+    <Menu.Menu position='right'>
+      <UserControls />
+    </Menu.Menu>
   </Menu>
 );
