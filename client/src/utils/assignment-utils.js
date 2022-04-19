c4399328ec73e5b9c8a96d5ef307dd2c8c8d3420
David <borland@renci.org>
2022-04-19 10:59:15 -0400

    Fix issue with hasActive assignments

--- client/src/utils/assignment-utils.js
+++ client/src/utils/assignment-utils.js
@@ -1,2 +1,2 @@
 export const hasActive = assignments => 
-  assignments && assignments.filter(assignment => !assignment.pending);
+  assignments && assignments.filter(assignment => !assignment.pending).length > 0;
