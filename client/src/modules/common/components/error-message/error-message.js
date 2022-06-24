63af625602e3717ea2785221753a4cd7e4c5bcdd
David Borland <borland@renci.org>
2022-06-24 13:32:41 -0400

    Add more info to error message (#96)

--- client/src/modules/common/components/error-message/error-message.js
+++ client/src/modules/common/components/error-message/error-message.js
@@ -4,6 +4,29 @@ import { ErrorContext, CLEAR_ERROR } from 'contexts';
 
 const { Header, Content } = Message;
 
+const composeError = error => {
+  if (!error) return null;
+
+  const m1 = error.message;
+  const m2 = error.response?.data?.message;
+
+  if (typeof error === 'string' || error instanceof String) {
+    return error;
+  }
+  else if (m1 || m2) {
+    const split = (message, n) => message.split(`\n`).map((s, i) => <p key={ n + i }>{ s }</p>);
+
+    let text = [];
+    if (m1) text = text.concat(split(m1, text.length));
+    if (m2) text = text.concat(split(m2, text.length));
+
+    return text;
+  }
+  else {
+    return JSON.stringify(error, Object.getOwnPropertyNames(error));
+  }
+};
+
 export const ErrorMessage = () => {
   const [{ error }, dispatch] = useContext(ErrorContext);
 
@@ -11,10 +34,7 @@ export const ErrorMessage = () => {
     dispatch({ type: CLEAR_ERROR });
   };
   
-  const text = !error ? null :
-    (typeof error === 'string' || error instanceof String) ? error :
-    error.message ? error.message.split(`\n`).map((s, i) => <p key={ i }>{ s }</p>) :
-    JSON.stringify(error, Object.getOwnPropertyNames(error));
+  const text = composeError(error);
 
   return (
     <Dimmer 
