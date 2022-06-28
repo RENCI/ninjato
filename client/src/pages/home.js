6101121dd98c8df4d28dc56d2a0ecec3dcacddfa
David <borland@renci.org>
2022-06-28 14:29:08 -0400

    Adjust timing

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -8,19 +8,24 @@ export const Home = () => {
       <div className={ styles.wrapper }>
         <div className={ styles.wrapper }>
           <Image src='https://images.squarespace-cdn.com/content/v1/55d80033e4b02dba43ff8058/1563373073348-ZQUETA5SCNUQLCPSB6O6/OlehKrupaWTmousecortex.jpg' />
-          <div className={ styles.text + " " + styles.text1 }>
-            With ~100 million nuclei, no human can count all the cells in a mouse brain
+          <div>
+            <div className={ styles.text }>
+              With ~100 million nuclei, no human can count all the cells in a mouse brain
+            </div>
+            <div className={ styles.text }>
+              We need to train an image recognition algorithm to recognize what a nucleus looks like
+            </div>
+            <div className={ styles.text }>
+              This requires many manually labeled nuclear samples, so we've developed ninjatō to make this easier to do
+            </div>
+            <div className={ styles.text }>
+              This tool will enable us to quantify differences in brain structure caused by mutations associated with neuropsychiatric disorders
+            </div>
+            <div className={ styles.text }>
+              You can help
+            </div>
           </div>
-          <div className={ styles.text + " " + styles.text2 }>
-            We need to train an image recognition algorithm to recognize what a nucleus looks like
-          </div>
-          <div className={ styles.text + " " + styles.text3 }>
-            This requires many manually labeled nuclear samples, so we've developed ninjatō to make this easier to do
-          </div>
-          <div className={ styles.text + " " + styles.text4 }>
-            This tool will enable us to quantify differences in brain structure caused by mutations associated with neuropsychiatric disorders
-          </div>
-        </div>      
+        </div>
         <Divider hidden>To continue...</Divider>
         <Segment basic>
           <Grid columns={2} stackable relaxed='very'>
