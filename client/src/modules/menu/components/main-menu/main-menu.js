28f7d82b9ea8e39b043c9aef2d265d6041dcfa2e
David <borland@renci.org>
2022-06-28 20:39:27 -0400

    Add links to menu

--- client/src/modules/menu/components/main-menu/main-menu.js
+++ client/src/modules/menu/components/main-menu/main-menu.js
@@ -1,17 +1,27 @@
+import { Link, useLocation } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 import { UserControls } from 'modules/menu/components/user-controls';
 
 const { Item } = Menu;
 
-export const MainMenu = () => (
-  <Menu borderless inverted attached>
-    <Item as={ Link } to='/' header>
-      <img alt="logo" src='logo192.png' />
-      ninjatō
-    </Item>
-    <Menu.Menu position='right'>
-      <UserControls />
-    </Menu.Menu>
-  </Menu>
-);
+export const MainMenu = () => {
+  const location = useLocation();
+
+  return (
+    <Menu borderless inverted attached>
+      <Item as={ Link } to='/' header active={ location.pathname === '/' }>
+        <img alt="logo" src='logo192.png' />
+        ninjatō
+      </Item>
+      <Item as={ Link } to='/select' active={ location.pathname === '/select' }>
+        Select assignment
+      </Item>
+      <Item as={ Link } to='/assignment' active={ location.pathname === '/assignment' }>
+        Current assignment
+      </Item>
+      <Menu.Menu position='right'>
+        <UserControls />
+      </Menu.Menu>
+    </Menu>
+  );
+};
