8009376da6190ce00a96e275abdb9af7e053aaa3
David Borland <borland@renci.org>
2022-09-01 15:49:05 -0400

    Work on region info appearance

--- client/src/modules/region/components/region-info/region-info.js
+++ client/src/modules/region/components/region-info/region-info.js
@@ -1,13 +1,19 @@
+import { RegionIcon } from 'modules/region/components/region-icon';
+import { Label, Icon } from 'semantic-ui-react';
+
 export const RegionInfo = ({ region }) => {
 
   console.log(region);
 
   return !region ? null : (
     <>
-      <div>{ region.label }</div>
+      <div><RegionIcon region={ region } /></div>
       { region.info &&
         <>
-          <div>{ region.info.status }</div>
+          <Label>
+            <Icon name='lock open' /> 
+            { region.info.status }
+          </Label>
         </>
       }
     </>
