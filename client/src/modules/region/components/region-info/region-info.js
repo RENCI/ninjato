4f18d11b0920d8d88fa322926cb985ff274239ac
David Borland <borland@renci.org>
2022-09-06 15:28:47 -0400

    Add status detail

--- client/src/modules/region/components/region-info/region-info.js
+++ client/src/modules/region/components/region-info/region-info.js
@@ -6,13 +6,17 @@ const { Item, Content, Header, Description, Icon } = List;
 const statusIcon = status =>
   status === 'inactive' ? 'lock open' : 'lock';
 
-const infoItem = (header, description, icon = null) => (
+const statusDetail = status => 
+  status === 'inactive' ? 'available to claim' : null;
+
+const infoItem = (header, description, detail = null, icon = null) => (
   <Item>
     <Content>
       <Header>{ header }</Header>
       <Description>
         { icon && <Icon name={ icon } /> }
         { description }
+        { detail && <div style={{ fontSize: 'x-small', color: '#999' }}>{ detail }</div> }
       </Description>
     </Content>
   </Item>
@@ -26,9 +30,9 @@ export const RegionInfo = ({ region }) => {
         <>
           <Divider fitted />
           <List horizontal>
-            { region.info.status && infoItem('Status', region.info.status, statusIcon(region.info.status)) }
-            { region.info.annotator && infoItem('User', region.info.annotator.login) }
-            { region.info.reviewer && infoItem('Reviewer', region.info.reviewer.login) } 
+            { region.info.status && infoItem('Status', region.info.status, statusDetail(region.info.status), statusIcon(region.info.status)) }
+            { region.info.annotator?.login && infoItem('User', region.info.annotator.login) }
+            { region.info.reviewer?.login && infoItem('Reviewer', region.info.reviewer.login) } 
           </List>
         </>
       }
