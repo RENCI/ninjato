a709afc3092d5eb3b04d4d072327885527449731
David Borland <borland@renci.org>
2022-11-15 15:05:17 -0500

    Accept command line argument for database to use, default to test_data.gz

--- up.sh
+++ up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker-compose up --build -d -V
-echo "INFO: allow girder and girder-mongodb containers to stand up before restoring d
-ata"
+echo "INFO: allow girder and girder-mongodb containers to stand up before restoring data"
 for pc in $(seq 10 -1 1); do
     echo -ne "$pc ...\033[0K\r" && sleep 1;
 done
-#sh girder/restore_db.sh girder/test_data.gz
-sh girder/restore_db.sh girder/server_data_11_04.gz
+
+data=${1:-"girder/test_data.gz"}
+sh girder/restore_db.sh $data
