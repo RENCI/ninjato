99a7755fa9ebd3a3e5ff7165403f88624e3009c3
David Borland <borland@renci.org>
2022-11-16 16:47:48 -0500

    Work on timeline counts

--- client/src/pages/progress.js
+++ client/src/pages/progress.js
@@ -5,7 +5,7 @@ import { RedirectMessage } from 'modules/common/components/redirect-message';
 import { api } from 'utils/api';
 
 const sortActions = (a, b) => a.time - b.time;
-
+/*
 const getUserTimelines = (user, volumes) => {
   const timelines = volumes.map(volume => 
     Object.entries(volume.history).reduce((timeline, [assignmentId, assignmentHistory]) => {
@@ -23,7 +23,7 @@ const getUserTimelines = (user, volumes) => {
     combined: timelines.flat().sort(sortActions)
   }
 };
-
+*/
 const getTimelineCounts = timeline => 
   timeline.reduce((counts, action) => {
     /*
@@ -49,30 +49,55 @@ const getTimelineCounts = timeline =>
    return counts;
   }, []);
 
-const getUserAssignments = (users, volumes) => {
+const getUserTimelines = (users, volumes) => {
   console.log(volumes);
 
-  const assignments = users.map(user => ({ user: user }));
+  const timelines = users.map(user => ({ user: user, timeline: [] }));
 
   volumes.forEach(volume => Object.entries(volume.history)
     .forEach(([assignmentId, assignmentHistory]) => {            
       let currentUser = null;
       assignmentHistory.forEach(action => {
-        if (action.type === 'annotation_assigned_to') {
-          currentUser = users.find(user => user.login === action.user);
+
+
+        // Check logic for first action here
+        
+
+        if (action.type === 'annotation_assigned_to' || 
+            action.type === 'annotation_completed_by' || 
+            action.type === 'annotation_rejected_by') {
+
+
+          if (action.type === 'annotation_completed_by') console.log(assignmentId)
+
+          currentUser = timelines.find(({ user }) => user.login === action.user);
 
           if (!currentUser) {
             console.warn(`Unknown user: ${ action.user }`);
           }
         }
- 
         
-
+        if (!currentUser) {
+          console.warn('No current user', assignmentHistory, action);
+          return;
+        }
         
+        currentUser.timeline.push(action);
         
+        if (action.type === 'annotation_rejected_by') {
+          currentUser = null;  
+        }       
       });
     })
   );
+
+  console.log(timelines);
+
+  timelines.forEach(user => {
+    user.counts = user.timeline.reduce((counts, action) => {
+
+    }, []);
+  });
 };
 
 export const Progress = () => {
@@ -100,10 +125,10 @@ export const Progress = () => {
       );
       */
 
-      const userAssignments = getUserAssignments(users, volumes);
+      const timelines = getUserTimelines(users, volumes);
 
       //console.log(timelines);
-      console.log(userAssignments);
+      console.log(timelines);
     }
      
     if (volumes) getUsers();
