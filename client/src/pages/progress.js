ccd55db71aa7ab86600af88beccef9d891ed7af6
David Borland <borland@renci.org>
2022-11-18 15:52:25 -0500

    Streamgraph

--- client/src/pages/progress.js
+++ client/src/pages/progress.js
@@ -1,110 +1,125 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ResponsiveStream } from '@nivo/stream';
+import { ResponsiveLine } from '@nivo/line';
 import { UserContext } from 'contexts';
 import { RedirectMessage } from 'modules/common/components/redirect-message';
 import { api } from 'utils/api';
 
-const sortActions = (a, b) => a.time - b.time;
-/*
-const getUserTimelines = (user, volumes) => {
-  const timelines = volumes.map(volume => 
-    Object.entries(volume.history).reduce((timeline, [assignmentId, assignmentHistory]) => {
-      return [
-        ...timeline,
-        ...assignmentHistory
-          .filter(action => action.user === user.login)
-          .map(action => ({ ...action, time: new Date(action.time), assignmentId: assignmentId }))          
-      ]
-    }, []).sort(sortActions)
+// XXX: Necessary to fix issues in assignment history. 
+// Can probably be removed after first volume (purple_box) is completed.
+const sanitizeHistory = volumes => 
+  volumes.forEach(({ history }) => 
+    Object.values(history).forEach(assignment => {
+      for (let i = 0; i < assignment.length; i++) {
+        const action = assignment[i];
+
+        if (
+          i === 0 && 
+          action.type !== 'annotation_assigned_to'
+        ) {
+          assignment.unshift({
+            ...action,
+            type: 'annotation_assigned_to'
+          });
+        }
+        else if (
+          action.type === 'annotation_rejected_by' && 
+          i < assignment.length - 1 && 
+          assignment[i + 1].type !== 'annotation_assigned_to'
+        ) {
+          assignment.splice(i + 1, 0, {
+            ...assignment[i + 1],
+            type: 'annotation_assigned_to'
+          });
+        }
+      }
+    })
   );
 
-  return {
-    volumes: timelines,
-    combined: timelines.flat().sort(sortActions)
-  }
-};
-*/
-const getTimelineCounts = timeline => 
-  timeline.reduce((counts, action) => {
-    /*
-    const count = counts.length === 0 ? 
-      { active: 0, completed: 0, declined: 0} :
-      {...counts[counts.length - 1]};
-
-    const current = 
-
-    if (counts.length === 0) {
-      counts.push({ 
-        time: action.time,
-        active: 0,
-        completed: 0,
-        declined: 0
-      });
-    }
-    else {
-      const previous = c
-      counts.push
-    }
-    */
-   return counts;
-  }, []);
-
 const getUserTimelines = (users, volumes) => {
-  console.log(volumes);
-
   const timelines = users.map(user => ({ user: user, timeline: [] }));
 
-  volumes.forEach(volume => Object.entries(volume.history)
-    .forEach(([assignmentId, assignmentHistory]) => {            
+  volumes.forEach(({ history }) => Object.values(history)
+    .forEach(assignmentHistory => {            
       let currentUser = null;
-      assignmentHistory.forEach(action => {
+      assignmentHistory.forEach((action, i, a) => {
+        action.time = new Date(action.time);
 
-
-        // Check logic for first action here
-        
-
-        if (action.type === 'annotation_assigned_to' || 
-            action.type === 'annotation_completed_by' || 
-            action.type === 'annotation_rejected_by') {
-
-
-          if (action.type === 'annotation_completed_by') console.log(assignmentId)
-
-          currentUser = timelines.find(({ user }) => user.login === action.user);
-
+        // Only add the first annotation_assigned_to
+        if (action.type === 'annotation_assigned_to') {
           if (!currentUser) {
-            console.warn(`Unknown user: ${ action.user }`);
+            currentUser = timelines.find(({ user }) => user.login === action.user);
+  
+            if (currentUser) {
+              currentUser.timeline.push(action);
+            }
+            else {
+              console.warn(`Unknown user: ${ action.user }`);
+            }
           }
         }
+        else {        
+          if (!currentUser) {
+            console.warn('No current user', assignmentHistory, action);
+            return;
+          }
         
-        if (!currentUser) {
-          console.warn('No current user', assignmentHistory, action);
-          return;
+          // XXX: Hack to simulate completed status until that is fixed on the server
+          // XXX: Move to sanitizeHistory
+          if (action.type === 'review_completed_by' && i === a.length - 1) {
+            currentUser.timeline.push({...action, type: 'review_verified_by' });
+          }
+          else {
+            currentUser.timeline.push(action);
+          }
+          
+          if (action.type === 'annotation_rejected_by') {
+            currentUser = null;  
+          }       
         }
-        
-        currentUser.timeline.push(action);
-        
-        if (action.type === 'annotation_rejected_by') {
-          currentUser = null;  
-        }       
       });
     })
   );
 
-  console.log(timelines);
+  timelines.forEach(({ timeline }) => timeline.sort((a, b) => a.time - b.time));
 
   timelines.forEach(user => {
-    user.counts = user.timeline.reduce((counts, action) => {
+    user.counts = user.timeline.reduce((counts, action, i, a) => {
+      const count = i === 0 ? {
+        active: 0,
+        review: 0,
+        completed: 0,
+        declined: 0
+      } : {...counts[i - 1]};
 
+      count.time = new Date(action.time);
+
+      switch (action.type) {
+        case 'annotation_assigned_to': count.active++; break;
+        case 'annotation_completed_by': count.active--; count.review++; break;
+        case 'annotation_rejected_by': count.active--; count.declined++; break;
+        case 'review_assigned_to': break;
+        case 'review_completed_by': count.review--; count.active++; break;
+        case 'review_verified_by': count.review--; count.completed++; break;
+        case 'review_rejected_by': break;
+        default: 
+          console.warn(`Unknown action type ${ action.type }`);
+      }
+
+      counts.push(count);
+
+      return counts;
     }, []);
   });
+
+  return timelines;
 };
 
 export const Progress = () => {
   const [{ user, volumes }] = useContext(UserContext);
+  const [timelines, setTimelines] = useState();
   const navigate = useNavigate();
-
-  console.log(volumes);
 
   useEffect(() => {
     if (!user) {
@@ -113,36 +128,61 @@ export const Progress = () => {
   }, [user, navigate]);
 
   useEffect(() => {
-    const getUsers = async () => {
+    const getData = async () => {
       const users = await api.getUsers();
+      const volumes = await api.getVolumes();
 
-      /*
-      const timelines = users.map(user => 
-        ({ 
-          user: user,
-          timelines: getUserTimelines(user, volumes)
-        })
-      );
-      */
+      sanitizeHistory(volumes);
 
       const timelines = getUserTimelines(users, volumes);
-
-      //console.log(timelines);
       console.log(timelines);
+
+      setTimelines(timelines);
     }
-     
-    if (volumes) getUsers();
+    
+    getData();
   }, [volumes]);
+
+  const index = 10;
+  const keys = ['active', 'review', 'completed', 'declined'];
+
+  console.log(timelines);
+
+  const data = timelines ? keys.map(key => ({
+    id: key,
+    data: timelines[index].counts.map((count, i) => ({ x: count.time.toISOString(), y: count[key] }))
+  })) : null;
+
+  console.log(data)
 
   return (
     !user ?
       <RedirectMessage message='No User' />
     :
-      <>
-        <div>user progress</div>
-        { /*user.reviewer*/user && 
-          <div>all progress</div>
+      <div style={{ height: 500 }}>
+        { timelines &&
+          /*
+          <ResponsiveLine 
+            data={ keys.map(key => ({
+              id: key,
+              data: timelines[index].counts.map(count => ({ x: count.time.toISOString(), y: count[key] }))
+            }))}
+            xScale={{ 
+              type: 'time',
+              format: 'YYYY-MM-DDTHH:mm:ss.sssZ'
+            }}
+            xFormat={ 'time:YYYY-MM-DDTHH:mm:ss.sssZ' }
+            axisBottom={ null }
+          />
+          */          
+          <ResponsiveStream 
+            data={ timelines[index].counts }
+            keys={ ['active', 'review', 'completed', 'declined'] }
+            offsetType='diverging'
+            order='reverse'
+            curve='monotoneY'
+          />          
         }
-      </>
+      </div>
   );
 };
