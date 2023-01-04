524795617d80abb68e7d37ec6b6b541f72f42269
David Borland <borland@renci.org>
2023-01-04 16:18:34 -0500

    Add dice score calculation

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -70,3 +70,22 @@ export const getMissingRegions = (maskData, regions) => {
     return missing;
   }, []);
 };
+
+export const diceScore = (image1, image2) => {  
+  const d1 = image1.getDimensions();
+  const d2 = image2.getDimensions();
+
+  if (d1[0] !== d2[0] || d1[1] !== d2[1] || d1[2] !== d2[2]) {
+    console.warn(`Images have different dimensions: [${ d1 }], [${ d2 }]`);
+    return null;
+  }
+
+  const s1 = image1.getPointData().getScalars().getData();
+  const s2 = image2.getPointData().getScalars().getData();
+
+  const ix = s1.reduce((ix, v, i) => {
+    return ix + Math.min(v, 1) * Math.min(s2[i], 1);
+  }, 0);
+
+  return 2 * ix / (s1.length + s2.length); 
+};
