a49e997bc2b0be1216492750591b6666c52ca6b4
David Borland <borland@renci.org>
2023-01-26 11:01:24 -0500

    Showing graphs with refactored volume progress

--- client/src/modules/progress/components/volume-progress/volume-progress.js
+++ client/src/modules/progress/components/volume-progress/volume-progress.js
@@ -1,106 +1,130 @@
-import { useContext } from 'react';
-import { Button, Modal, Tab, Menu, Header } from 'semantic-ui-react';
-import { UserContext, SET_ACTIVE_REGION } from 'contexts';
-import { CommentHistory } from 'modules/comment/components/comment-history';
-import { RegionIcon } from 'modules/region/components/region-icon';
-
-const { Content } = Modal;
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Tab, Menu } from 'semantic-ui-react';
+import { UserContext } from 'contexts';
+import { RedirectMessage } from 'modules/common/components/redirect-message';
+import { VegaWrapper } from 'modules/vega/components/vega-wrapper';
+import { api } from 'utils/api';
+import { lineChart, stackedArea } from 'vega-specs';
 
 // XXX: Necessary to fix issues in assignment history. 
 // Can probably be removed after first volume (purple_box) is completed.
-const sanitizeHistory = volumes => 
-  volumes.forEach(({ history }) => 
-    Object.values(history).forEach(assignment => {
-      for (let i = 0; i < assignment.length; i++) {
-        const action = assignment[i];
+const sanitizeHistory = volume => {
+  console.log(volume);
+  Object.values(volume.history).forEach(assignment => {
+    for (let i = 0; i < assignment.length; i++) {
+      const action = assignment[i];
 
-        action.time = new Date(action.time);
+      action.time = new Date(action.time);
 
-        if (
-          i === 0 && 
-          action.type !== 'annotation_assigned_to'
-        ) {
-          assignment.unshift({
-            ...action,
-            type: 'annotation_assigned_to'
-          });
-        }
-        else if (
-          action.type === 'annotation_rejected_by' && 
-          i < assignment.length - 1 && 
-          assignment[i + 1].type !== 'annotation_assigned_to'
-        ) {
-          assignment.splice(i + 1, 0, {
-            ...assignment[i + 1],
-            type: 'annotation_assigned_to'
-          });
-        }
-        else if (action.type === 'review_completed_by' && i === assignment.length - 1) {
-          action.type = 'review_verified_by';
-        }
+      if (
+        i === 0 && 
+        action.type !== 'annotation_assigned_to'
+      ) {
+        assignment.unshift({
+          ...action,
+          type: 'annotation_assigned_to'
+        });
       }
-    })
-  );
-
-const getVolumeTimelines = volumes => {
-  const timelines = volumes.map(volume => ({ volume: volume, timeline: [] }));
-
-  volumes.forEach(({ history }, i) => {
-    const timeline = timelines[i].timeline;
-
-    Object.values(history).forEach(assignment => {
-      assignment.forEach((action, i) => {
-        // Only add the first annotation_assigned_to
-        if (i === 0 && action.type !== 'annotation_assigned_to') {
-          console.warn('Assigned to not first action: ', assignment);
-        }
-        else {     
-          timeline.push(action);  
-        }
-      });
-    }); 
-  });
-
-  timelines.forEach(({ timeline }) => timeline.sort((a, b) => a.time - b.time));
-
-  timelines.forEach(volume => {
-    volume.counts = volume.timeline.reduce((counts, action, i, a) => {
-      const count = i === 0 ? {
-        active: 0,
-        review: 0,
-        completed: 0,
-        declined: 0,
-        reviewDeclined: 0
-      } : {...counts[i - 1]};
-
-      count.time = new Date(action.time);
-
-      switch (action.type) {
-        case 'annotation_assigned_to': count.active++; break;
-        case 'annotation_completed_by': count.active--; count.review++; break;
-        case 'annotation_rejected_by': count.active--; count.declined++; break;
-        case 'review_assigned_to': break;
-        case 'review_completed_by': count.review--; count.active++; break;
-        case 'review_verified_by': count.review--; count.completed++; break;
-        case 'review_rejected_by': count.review--; count.reviewDeclined++; break;
-        default: 
-          console.warn(`Unknown action type ${ action.type }`);
+      else if (
+        action.type === 'annotation_rejected_by' && 
+        i < assignment.length - 1 && 
+        assignment[i + 1].type !== 'annotation_assigned_to'
+      ) {
+        assignment.splice(i + 1, 0, {
+          ...assignment[i + 1],
+          type: 'annotation_assigned_to'
+        });
       }
-
-      counts.push(count);
-
-      return counts;
-    }, []);
+      else if (action.type === 'review_completed_by' && i === assignment.length - 1) {
+        action.type = 'review_verified_by';
+      }
+    }
   });
-
-  return timelines;
 };
 
+const getVolumeTimeline = volume => {
+  const timeline = [];
 
-export const VolumeProgress = volume => {
-  const [timeline, setTimeline] = useState();
+  Object.values(volume.history).forEach(assignment => {
+    assignment.forEach((action, i) => {
+      // Only add the first annotation_assigned_to
+      if (i === 0 && action.type !== 'annotation_assigned_to') {
+        console.warn('Assigned to not first action: ', assignment);
+      }
+      else {     
+        timeline.push(action);  
+      }
+    });
+  });
+
+  timeline.sort((a, b) => a.time - b.time);
+
+  timeline.counts = timeline.reduce((counts, action, i) => {
+    const count = i === 0 ? {
+      active: 0,
+      review: 0,
+      completed: 0,
+      declined: 0,
+      reviewDeclined: 0
+    } : {...counts[i - 1]};
+
+    count.time = new Date(action.time);
+
+    switch (action.type) {
+      case 'annotation_assigned_to': count.active++; break;
+      case 'annotation_completed_by': count.active--; count.review++; break;
+      case 'annotation_rejected_by': count.active--; count.declined++; break;
+      case 'review_assigned_to': break;
+      case 'review_completed_by': count.review--; count.active++; break;
+      case 'review_verified_by': count.review--; count.completed++; break;
+      case 'review_rejected_by': count.review--; count.reviewDeclined++; break;
+      default: 
+        console.warn(`Unknown action type ${ action.type }`);
+    }
+
+    counts.push(count);
+
+    return counts;
+  }, []);
+
+  return timeline;
+};  
+
+export const VolumeProgress = ({ volume, users }) => {
+  const [volumeTimeline, setVolumeTimeline] = useState();
+  //const [userTimelines, setUserTimelines] = useState();
+
+  useEffect(() => {
+    if (volume && users) {
+      sanitizeHistory(volume);    
+      setVolumeTimeline(getVolumeTimeline(volume));
+    // setUserTimelines(getUserTimelines(users, volume));
+    }
+  }, [volume, users]);
+
+  const keys = ['declined', 'reviewDeclined', 'completed', 'review', 'active'];
+  const keyIndex = keys.reduce((keyIndex, key, i) => {
+    keyIndex[key] = i;
+    return keyIndex;
+  }, {});
+  
+  const lineData = volumeTimeline ? volumeTimeline.counts.reduce((data, count) => {
+    return [
+      ...data,
+      ...keys.map(key => ({ count: count[key], time: count.time, status: key, order: keyIndex[key] }))
+    ]
+  }, []) : null;
+  
+  const areaData = volumeTimeline ? volumeTimeline.counts.reduce((data, count) => {
+    return [
+      ...data,
+      ...keys.map(key => ({ count: key.includes('declined') ? -count[key] : count[key], time: count.time, status: key, order: keyIndex[key] }))
+    ]
+  }, []) : null;
 
   return (
+    lineData && areaData && 
     <>
       <VegaWrapper spec={ lineChart } data={ lineData } />
       <VegaWrapper spec={ stackedArea } data={ areaData } />
--- client/src/pages/progress.js
+++ client/src/pages/progress.js
@@ -1,101 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Tab, Menu } from 'semantic-ui-react';
+import { Tab, Menu, Loader } from 'semantic-ui-react';
 import { UserContext } from 'contexts';
 import { RedirectMessage } from 'modules/common/components/redirect-message';
-import { VegaWrapper } from 'modules/vega/components/vega-wrapper';
+import { VolumeProgress } from 'modules/progress/components/volume-progress';
 import { api } from 'utils/api';
 import { lineChart, stackedArea } from 'vega-specs';
-
-// XXX: Necessary to fix issues in assignment history. 
-// Can probably be removed after first volume (purple_box) is completed.
-const sanitizeHistory = volumes => 
-  volumes.forEach(({ history }) => 
-    Object.values(history).forEach(assignment => {
-      for (let i = 0; i < assignment.length; i++) {
-        const action = assignment[i];
-
-        action.time = new Date(action.time);
-
-        if (
-          i === 0 && 
-          action.type !== 'annotation_assigned_to'
-        ) {
-          assignment.unshift({
-            ...action,
-            type: 'annotation_assigned_to'
-          });
-        }
-        else if (
-          action.type === 'annotation_rejected_by' && 
-          i < assignment.length - 1 && 
-          assignment[i + 1].type !== 'annotation_assigned_to'
-        ) {
-          assignment.splice(i + 1, 0, {
-            ...assignment[i + 1],
-            type: 'annotation_assigned_to'
-          });
-        }
-        else if (action.type === 'review_completed_by' && i === assignment.length - 1) {
-          action.type = 'review_verified_by';
-        }
-      }
-    })
-  );
-
-const getVolumeTimelines = volumes => {
-  const timelines = volumes.map(volume => ({ volume: volume, timeline: [] }));
-
-  volumes.forEach(({ history }, i) => {
-    const timeline = timelines[i].timeline;
-
-    Object.values(history).forEach(assignment => {
-      assignment.forEach((action, i) => {
-        // Only add the first annotation_assigned_to
-        if (i === 0 && action.type !== 'annotation_assigned_to') {
-          console.warn('Assigned to not first action: ', assignment);
-        }
-        else {     
-          timeline.push(action);  
-        }
-      });
-    }); 
-  });
-
-  timelines.forEach(({ timeline }) => timeline.sort((a, b) => a.time - b.time));
-
-  timelines.forEach(volume => {
-    volume.counts = volume.timeline.reduce((counts, action, i, a) => {
-      const count = i === 0 ? {
-        active: 0,
-        review: 0,
-        completed: 0,
-        declined: 0,
-        reviewDeclined: 0
-      } : {...counts[i - 1]};
-
-      count.time = new Date(action.time);
-
-      switch (action.type) {
-        case 'annotation_assigned_to': count.active++; break;
-        case 'annotation_completed_by': count.active--; count.review++; break;
-        case 'annotation_rejected_by': count.active--; count.declined++; break;
-        case 'review_assigned_to': break;
-        case 'review_completed_by': count.review--; count.active++; break;
-        case 'review_verified_by': count.review--; count.completed++; break;
-        case 'review_rejected_by': count.review--; count.reviewDeclined++; break;
-        default: 
-          console.warn(`Unknown action type ${ action.type }`);
-      }
-
-      counts.push(count);
-
-      return counts;
-    }, []);
-  });
-
-  return timelines;
-};
 
 const getUserTimelines = (users, volumes) => {
   const timelines = users.map(user => ({ user: user, timeline: [] }));
@@ -169,9 +79,9 @@ const getUserTimelines = (users, volumes) => {
 };
 
 export const Progress = () => {
-  const [{ user, volumes }] = useContext(UserContext);
-  const [volumeTimelines, setVolumeTimelines] = useState();
-  const [userTimelines, setUserTimelines] = useState();
+  const [{ user }] = useContext(UserContext);
+  const [users, setUsers] = useState();
+  const [volumes, setVolumes] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -181,57 +91,23 @@ export const Progress = () => {
   }, [user, navigate]);
 
   useEffect(() => {
-    console.log('getting data')
-
     const getData = async () => {
       const users = await api.getUsers();
+      setUsers(users);
+
       const volumes = await api.getVolumes();
-
-      console.log('got data');
-
-      sanitizeHistory(volumes);
-
-      console.log('sanitized')
-
-      setVolumeTimelines(getVolumeTimelines(volumes));
-
-      console.log ('got volume timelines')
-      setUserTimelines(getUserTimelines(users, volumes));
-
-      console.log('got user timeines')
+      setVolumes(volumes);
     }
     
     getData();
-  }, [volumes]);
+  }, []);
 
-  const index = 0;
-  const keys = ['declined', 'reviewDeclined', 'completed', 'review', 'active'];
-  const keyIndex = keys.reduce((keyIndex, key, i) => {
-    keyIndex[key] = i;
-    return keyIndex;
-  }, {});
-
-  //console.log(timelines);
-  //console.log(volumes);
-
-  const lineData = volumeTimelines ? volumeTimelines[index].counts.reduce((data, count) => {
-    return [
-      ...data,
-      ...keys.map(key => ({ count: count[key], time: count.time, status: key, order: keyIndex[key] }))
-    ]
-  }, []) : null;
-
-  const areaData = volumeTimelines ? volumeTimelines[index].counts.reduce((data, count) => {
-    return [
-      ...data,
-      ...keys.map(key => ({ count: key.includes('declined') ? -count[key] : count[key], time: count.time, status: key, order: keyIndex[key] }))
-    ]
-  }, []) : null;
-console.log(volumes);
   return (
     !user ?
       <RedirectMessage message='No User' />
-    : volumes &&
+    : !volumes ? 
+      <Loader active />
+    :
       <Tab
         menu={{ secondary: true, pointing: true, attached: 'top' }}
         panes={[
@@ -243,10 +119,7 @@ console.log(volumes);
                 panes={ volumes.map(volume => ({    
                   menuItem: <Menu.Item>{ volume.name }</Menu.Item>,
                   render: () =>
-                    <>
-                      <VegaWrapper spec={ lineChart } data={ lineData } />
-                      <VegaWrapper spec={ stackedArea } data={ areaData } />
-                    </>
+                    <VolumeProgress volume={ volume } users={ users } />
                 }))}
               />
           },
