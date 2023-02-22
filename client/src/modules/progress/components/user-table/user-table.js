a7386c562444cc2a2b0d617fb8459c657268445c
David Borland <borland@renci.org>
2023-02-22 12:08:06 -0500

    Add sorting, filter users with no data

--- client/src/modules/progress/components/user-table/user-table.js
+++ client/src/modules/progress/components/user-table/user-table.js
@@ -1,3 +1,4 @@
+import { useState, useMemo } from 'react';
 import { Table } from 'semantic-ui-react';
 
 const { Header, Body, Row, HeaderCell, Cell } = Table;
@@ -5,60 +6,92 @@ const { Header, Body, Row, HeaderCell, Cell } = Table;
 const columns = [
   {
     header: 'User',
-    value: d => d.user.login
+    value: 'user',
+    cellValue: d => d.user.login
   }
 ];
 
 export const UserTable = ({ users }) => {
-  console.log(users);
+  const [sortColumn, setSortColumn] = useState(columns[0].value);
+  const [sortDirection, setSortDirection] = useState('ascending');
 
-  const times = users.reduce((times, user) => {
-    user.counts?.forEach(count => {
-      const time = count.time.getTime();
+  const times = useMemo(() => (
+    users.reduce((times, user) => {
+      user.counts?.forEach(count => {
+        const time = count.time.getTime();
+  
+        if (!times.includes(time)) {
+          times.push(time);
+        }
+      });
+  
+      return times;
+    }, []).sort((a, b) => b - a)
+  ), [users]);
 
-      if (!times.includes(time)) {
-        times.push(time);
-      }
-    });
-
-    return times;
-  }, []).sort((a, b) => b - a);
-
-  const allColumns = [
+  const allColumns = useMemo(() => ([
     ...columns,
     ...times.map(time => ({
       header: new Date(time).toDateString(),
-      value: d => {
+      value: time,
+      cellValue: d => {
         const count = d.counts?.find(count => count.time.getTime() === time);
-        return count ? count.completed : '';
+        return count ? count.completed : null;
       }
     }))
-  ];
+  ]), [times]);
+
+  const data = useMemo(() => (
+    users.filter(user => user.counts).map(user => {
+      const u = {};
+      allColumns.forEach(column => u[column.value] = column.cellValue(user));
+      return u;
+    })
+  ), [users, allColumns]);
+
+  const onColumnClick = column => {
+    if (column === sortColumn) {
+      setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending');
+    }
+    else {
+      setSortDirection('ascending');
+    }
+
+    setSortColumn(column);
+  };
 
   return (
     <div style={{ width: '100%', overflowX: 'auto' }}>
-    <Table basic='very' compact>
-      <Header>
-        <Row>
-          { allColumns.map((column, i) => 
-            <HeaderCell key={ i }>
-              { column.header }
-            </HeaderCell>
-          )}
-        </Row>        
-      </Header>
-      <Body>
-        { users.map((user, i) =>
-          <Row key={ i }>
+      <Table basic='very' compact sortable>
+        <Header>
+          <Row>
             { allColumns.map((column, i) => 
-              <Cell key={ i }>
-                { column.value(user) }
-              </Cell>
+              <HeaderCell 
+                key={ i }
+                sorted={ column.value === sortColumn ? sortDirection : null } 
+                onClick={ () => onColumnClick(column.value) }
+              >
+                { column.header }
+              </HeaderCell>
             )}
-          </Row>
-        )}
-      </Body>
-    </Table>
+          </Row>        
+        </Header>
+        <Body>
+          { data.sort((a, b) => {
+            const va = a[sortColumn];
+            const vb = b[sortColumn];
+            return sortDirection === 'ascending' ? (va > vb ? 1 : -1) : (va > vb ? -1 : 1);
+          }).map((user, i) =>
+            <Row key={ i }>
+              { allColumns.map((column, i) => 
+                <Cell key={ i }>
+                  { user[column.value] }
+                </Cell>
+              )}
+            </Row>
+          )}
+        </Body>
+      </Table>
     </div>
   );
 };
