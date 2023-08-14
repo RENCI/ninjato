9ce8ae323cb920acf98b490198ccfd7b3608fe09
David Borland <borland@renci.org>
2023-08-14 11:30:02 -0400

    Feature/client training info (#254)
    * Add training info
    
    * Remove unused parameters
    
    ---------
    
    Co-authored-by: hyi <hongyi@renci.org>

---
+++ client/src/modules/assignment/components/training-info/training-info.js
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { UserContext } from 'contexts';
+import styles from './styles.module.css';
+
+export const TrainingInfo = () => {
+  const [{ assignment }] = useContext(UserContext);
+
+  const info = assignment.trainingInfo;
+
+  const message = info.merge ? `merge region with ${ info.merge.length } neighboring region ${ info.merge.length > 1 ? 's' : '' }` :
+    info.split ? 'split region into two regions' :
+    'refine region boundary'; 
+
+  return (
+    <>
+      { !assignment.trainingInfo ? null :
+        <div className={ styles.trainingInfo }>
+          <span className={ styles.emphasize }>Training assignment: </span>
+          { message }
+        </div> 
+      }
+    </>
+  );
+};
